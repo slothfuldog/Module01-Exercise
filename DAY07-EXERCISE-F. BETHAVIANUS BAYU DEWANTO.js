1d6274cea281da8ab54f50bc7e656f6455c238de
slothfuldog <fransiskusbetha@gmail.com>
2022-11-14 22:15:21 +0700

    push review day 8 dan revisi day 7 question 5

--- DAY07-EXERCISE-F. BETHAVIANUS BAYU DEWANTO.js
+++ DAY07-EXERCISE-F. BETHAVIANUS BAYU DEWANTO.js
@@ -105,9 +105,7 @@ let factorialize = (num) => {
     if (num === 0) {
         return 1;
     }
-    for (let i = num - 1; i >= 1; i--) {
-        num *= i;
-    }
-    return num;
+    return num * factorialize(num - 1);
 };
 console.log(`faktorisasi dari ${key} adalah ${factorialize(key)}`);
+
---
+++ DAY08-REVIEW-F.BETHAVIANUS BAYU DEWANTO.js
@@ -0,0 +1,39 @@
+let myObj = [{
+    name: "Avanza",
+    brand: "Toyota",
+    machine: "DOHC",
+    cc: 1500
+}, {
+    name: "Supra",
+    brand: "Toyota",
+    machine: "V8",
+    cc: 5000
+}, {
+    name: "ERTIGA",
+    brand: "Suzuki",
+    machine: "DOHC",
+    cc: 1550
+}]
+
+let print = (data, callback) => {
+    str = '';
+    for (let key in data) {
+        let tempData = data[key]
+        let index = key;
+        const {
+            name,
+            brand,
+            machine,
+            cc
+        } = tempData;
+        if (callback) {
+            if (callback(data[index])) {
+                str += `${parseInt(key) + 1}. ${brand} ${name} with ${machine} ${cc.toLocaleString("id")}\n`;
+            }
+        } else {
+            str += `${parseInt(key) + 1}. ${brand} ${name} with ${machine} ${cc.toLocaleString("id")}\n`;
+        }
+    }
+    return str;
+}
+console.log(print(myObj, (myObj) => myObj.brand === "Toyota"));
